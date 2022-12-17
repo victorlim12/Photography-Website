b1640596483631b5e578c3b3391eae6faaafe57e
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2022-12-17 16:09:21 +0800

    test framer motion

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,27 +10,27 @@ import { Grid, Typography, Button } from "@mui/material";
 //import { motion, useAnimation } from "framer-motion";
 import { useAnimation, motion, useScroll, useTransform } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 
 export default function Hero(){
-    const { scrollYProgress } = useScroll();
-    const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+    // const { scrollYProgress } = useScroll();
+    // const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
 
     return(
         <div className="image-bg" 
                 style={{
                         transition: "all .5s ease",
                         WebkitTransition: "all 1s ease",
-                        MozTransition: "all 1s ease", minHeight:'200vh', overflow:'hidden'
+                        MozTransition: "all 1s ease", minHeight:'100vh', overflowY:'scroll'
                         }}>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-            <motion.div transition={{ delay: 1 }} style={{
-          scale
-        }}>
+            <motion.div   initial={{ opacity: 0, scale: 1.5}}
+            animate={{scale: 1, opacity: 1,transition: {
+          duration: 2 },
+      }}>
             <Grid container sx={{display:'block'}}>
-                <Grid item xs={12} sx={{ textAlign: 'center' }} >
-                    <Typography variant="h2" fontWeight={600} sx={{lineHeight:1, letterSpacing:7 }} ><span className="grey">WELCOME.</span> </Typography>
-                    <Typography variant="h2" >to a glimpse</Typography>
+                <Grid item sx={{ textAlign: 'center'}} >
+                    <Typography variant="h2" fontWeight={600} sx={{lineHeight:1, letterSpacing:7 }} ><span className="blue">WELCOME.</span> </Typography>
+                    <Typography variant="h2" >to a glimpse of</Typography>
                     <Typography variant="h2" >my sanity and <span className="crimson" style={{fontWeight: 600}}>insanity</span></Typography>
                     {/* <Button variant="contained" size="large" sx={{mt:'3%'}}>WHERE</Button> */}
                 </Grid>
